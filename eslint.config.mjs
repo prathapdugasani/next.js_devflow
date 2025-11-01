18bb21430f3dae445f929cd2c79a771b01e55542
prathapdugasani <prathapdugasani123@gmail.com>
2025-11-01 16:13:31 +0530

    implement eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,16 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+}); 
+
 const eslintConfig = defineConfig([
+  ...compat.config({
+    extends: ["standard","prettier"],
+  }),
   ...nextVitals,
   ...nextTs,
   // Override default ignores of eslint-config-next.
